9c932aa0131a9577a6a44e75231b9f0a952d4760
Danny Liang <d23liang@uwaterloo.ca>
2025-04-06 21:42:08 -0400

    Chore: add comments

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -10,13 +10,14 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  // // Fetch user from Supabase
+  // Fetch user from Supabase
   const { data: users, error } = await supabase
     .from("users")
     .select("*")
     .eq("email", email)
     .single();
 
+  // Ensure supabase query was processed
   if (error || !users) {
     return NextResponse.json({ error: "User not found" }, { status: 401 });
   }
@@ -31,11 +32,11 @@ export async function POST(req: Request) {
     response.cookies.set("auth_token", "", {
       expires: new Date(0),
       httpOnly: true,
-    }); // Expire the cookie
+    }); // Expire the cookie after session
     return response;
   }
 
-  // Set token (cookie) for authentication
+  // Cookie for authentication
   const response = NextResponse.json({ success: true });
   response.cookies.set("auth_token", "some_random_token", { httpOnly: true });
 
