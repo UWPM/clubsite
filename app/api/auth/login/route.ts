abe158f1299243aa48c541da50e51abc37f791ba
Danny Liang <d23liang@uwaterloo.ca>
2025-04-06 21:39:13 -0400

    Fix: fix bug to compare user pw and hash pw in supabase

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,40 +1,39 @@
 import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
-import { hashPassword } from "@/lib/auth"
+import bcrypt  from "bcryptjs"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  // Fetch user from Supabase
-  const { data: user, error } = await supabase
+  // // Fetch user from Supabase
+  const { data: users, error } = await supabase
     .from("users")
     .select("*")
     .eq("email", email)
     .single();
 
-  if (error || !user) {
+  if (error || !users) {
     return NextResponse.json({ error: "User not found" }, { status: 401 });
   }
 
   // Hashes password to comapre to supabase
-  const hashedPassword = hashPassword(password)
-  if (hashedPassword != user.password) {
-    return NextResponse.json({error: "Invalid password"}, {status: 401});
+  const passwordMatch = await bcrypt.compare(password, users.password);
+  if (!passwordMatch) {
+    const response = NextResponse.json(
+      { error: "Invalid password" },
+      { status: 401 },
+    );
+    return response;
   }
 
   // Set token (cookie) for authentication
   const response = NextResponse.json({ success: true });
   response.cookies.set("auth_token", "some_random_token", { httpOnly: true });
 
-  console.log(user.password)
-
-
   return response;
-  
 }
