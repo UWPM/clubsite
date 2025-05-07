670137f53aa17560f6bc5d688171733d87933a18
sunniekapar <kaparsunnie@gmail.com>
2025-05-06 17:41:17 -0700

    fix: always redirect to apply page

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["picsum.photos"], // ! THIS IS FOR TESTING
   },
+  async redirects() {
+    return [
+      {
+        source: "/((?!apply$).*)",
+        destination: "/apply",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
