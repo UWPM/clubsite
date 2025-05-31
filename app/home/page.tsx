33a0d76e1ee496d5fbf5c8308b5801ad8876110f
sunniekapar <kaparsunnie@gmail.com>
2025-05-31 15:20:45 -0700

    chore: remove unused imports

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,4 @@
-import Template from "@/components/template";
 import Hero from "./layouts/hero";
-import OurImpact from "./layouts/our-impact";
-import PastEvents from "./layouts/past-events";
-import WhatWeDo from "./layouts/what-we-do";
-import WorkExperience from "./layouts/work-experience";
 
 export default function Home() {
   return <Hero />;
