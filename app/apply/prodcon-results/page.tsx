aecdbf12d750ce53488ec443c95e2d76a0d0cb58
Gaurika-G <11g19g@gmail.com>
2025-12-10 18:05:50 -0500

    frontend design ProdCone results

---
+++ app/apply/prodcon-results/page.tsx
@@ -0,0 +1,11 @@
+import Template from "@/components/template";
+import ProdConResultsGrid from "./layouts/results-grid";
+
+export default function ProdConResults() {
+  return (
+    <Template>
+      <ProdConResultsGrid />
+    </Template>
+  );
+}
+
